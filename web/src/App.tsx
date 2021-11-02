daa59099d3fb1b811953a9697a15f306f3466c88
Jacqueline <maaaander@gmail.com>
2021-11-02 17:06:55 -0400

    feat(add boards): update active board and styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import axios from 'axios'
 
 import SectionI from './types/section'
-import BoardI from './types/board'
+import BoardI from './types/board';
 
 import List from './components/list'
 
@@ -19,6 +19,20 @@ export const BoardContainer = styled.div`
   padding: 5px;
   align-items: flex-start;
   flex-wrap: wrap;
+`
+export const ActiveBoardView = styled.div`
+  border: 2px solid white;
+  padding: 1%;
+  background-color: rgba(255, 255, 255, 0.1);
+`
+export const ActiveBoardTitle = styled.div`
+  color: white;
+  font-weight: 700;
+  font-size: 2em;
+`
+
+export const ActiveBoardError = styled.div`
+  color: white;
 `
 
 function App() {
@@ -144,40 +158,50 @@ function App() {
   type acceptableBoardTypes = BoardI | null
   const [boards, setBoards] = useState<BoardI[]>(initalBoardList)
   const [activeBoard, setActiveBoard] = useState<acceptableBoardTypes>(null)
-  const [isTempBoardActive, setIsTempBoardActive] = useState(false)
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/sections').then((response) => {
-      //Section order is determined by ID so sort by ID
-      const sortedSections = response.data.sort((a: SectionI, b: SectionI) => a.id - b.id)
-      setSections(sortedSections)
-    })
-  })
+  // useEffect(() => {
+  //   axios.get('http://localhost:3001/sections').then((response) => {
+  //     //Section order is determined by ID so sort by ID
+  //     const sortedSections = response.data.sort((a: SectionI, b: SectionI) => a.id - b.id)
+  //     setSections(sortedSections)
+  //   })
+  // })
 
   const onCardSubmit = (sectionId: number, title: string) => {
-    axios({
-      method: 'post',
-      url: 'http://localhost:3001/cards',
-      data: { sectionId, title }
-    }).then((response) => {
-      let sectionsClone: SectionI[] = [...sections]
-      for (let i = 0; i < sectionsClone.length; i++) {
-        let section: SectionI = sectionsClone[i]
-        if (section.id == sectionId) {
-          section.cards.push({
-            id: response.data.id,
-            title: response.data.title,
-            section_id: sectionId
-          })
-          setSections(sectionsClone)
-        }
-      }
-    })
+    // axios({
+    //   method: 'post',
+    //   url: 'http://localhost:3001/cards',
+    //   data: { sectionId, title }
+    // }).then((response) => {
+    //   let sectionsClone: SectionI[] = [...sections]
+    //   for (let i = 0; i < sectionsClone.length; i++) {
+    //     let section: SectionI = sectionsClone[i]
+    //     if (section.id == sectionId) {
+    //       section.cards.push({
+    //         id: response.data.id,
+    //         title: response.data.title,
+    //         section_id: sectionId
+    //       })
+    //       setSections(sectionsClone)
+    //     }
+    //   }
+    // }
+    console.log('card submitted')
+
+    }
+      
+      
+
+    let sectionsClone: SectionI[] = [...sections]
+    for (let i = 0; i < sectionsClone.length; i++) {
+      let section: SectionI = sectionsClone[i]
+      section.cards.push({ id: id, title: title, section_id: sectionId })
+      setSections(sectionsClone)
+    }
   }
 
   const handleBoardClick = (board_id: number) => {
     const selected_board = boards.filter((board) => board.id === board_id)[0]
-
     setActiveBoard(selected_board)
   }
 
@@ -191,12 +215,13 @@ function App() {
     //   url: 'http://localhost:3001/boards',
     //   data: { title }
     // }).then((response) => {
-    //   boards.push({
+    //   let boardsClone = [...boards]
+    //   boardsClone.push({
     //     id: response.data.id,
     //     title: response.data.title,
     //     items: response.data.sections
     //   })
-    //   setBoards(boards)
+    //   setBoards(boardsClone)
     // })
     let boardsClone = [...boards]
     const id = Math.max(...boardsClone.map((o) => o.id), 0) + 1
@@ -246,19 +271,28 @@ function App() {
           title='Your Boards'
         ></List>
       </BoardContainer>
-      {!!activeBoard &&
-        !!activeBoard.sections.length &&
-        activeBoard.sections.map((section: SectionI) => {
-          return (
-            <List
-              itemsList={section.cards}
-              typeName='card'
-              onItemSubmit={onCardSubmit}
-              handleSelect={handleCardClick}
-              title={section.title}
-            ></List>
-          )
-        })}
+      <BoardContainer>
+        {!!activeBoard && !!activeBoard.sections.length && (
+          <ActiveBoardView>
+            <ActiveBoardTitle>{activeBoard.title}</ActiveBoardTitle>
+            {activeBoard.sections.map((section: SectionI) => {
+              return (
+                <List
+                  itemsList={section.cards}
+                  typeName='card'
+                  onItemSubmit={onCardSubmit}
+                  handleSelect={handleCardClick}
+                  title={section.title}
+                ></List>
+              )
+            })}
+          </ActiveBoardView>
+        )}
+        {!activeBoard && <ActiveBoardError>Please select a board</ActiveBoardError>}
+        {!!activeBoard && !activeBoard.sections.length && (
+          <ActiveBoardError>This board has no data to display</ActiveBoardError>
+        )}
+      </BoardContainer>
     </React.Fragment>
   )
 }
--- web/src/types/card.tsx
+++ web/src/types/card.tsx
@@ -2,5 +2,4 @@ export default interface CardI {
   id: number
   title: string
   section_id: number
-  items?: any //TODO remove
 }
